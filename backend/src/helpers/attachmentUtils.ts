2885272839b2d67dfaaba34b495894842e78dd24
ice-devs <othukeotubu@gmail.com>
2022-10-10 06:16:15 +0100

    new commit

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -1,6 +1,8 @@
 import * as AWS from 'aws-sdk'
+const AWSXRay = require('aws-xray-sdk')
 // import * as AWSXRay from 'aws-xray-sdk'
 // const XAWS = AWSXRay.captureAWS(AWS)
+const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
 
@@ -8,7 +10,7 @@ export class AttachmentUtils{
     constructor(
         private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
         private readonly urlExpiration = Number(process.env.SIGNED_URL_EXPIRATION),
-        private readonly s3 = new AWS.S3({ signatureVersion : 'v4' })){
+        private readonly s3 = new XAWS.S3({ signatureVersion : 'v4' })){
         }
 
     async createAttachmentPresignedUrl(todoId: string) {
--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -1,18 +1,18 @@
 import * as AWS from 'aws-sdk'
-// import * as AWSXRay from 'aws-xray-sdk'
-import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+const AWSXRay = require('aws-xray-sdk')
+// import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 // import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
 import { TodoUpdate } from '../models/TodoUpdate';
 
-// const XAWS = AWSXRay.captureAWS(AWS)
+const XAWS = AWSXRay.captureAWS(AWS)
 
 // const logger = createLogger('TodosAccess')
 
 // TODO: Implement the dataLayer logic
 export class TodosAccess{
     constructor(
-        private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
+        private readonly docClient = new XAWS.DynamoDB.DocumentClient(),
         private readonly todosTable = process.env.TODOS_TABLE,
         private readonly indexName = process.env.TODOS_CREATED_AT_INDEX){
         }
